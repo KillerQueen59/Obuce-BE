ff2cb12dca587abb932b639a773413f1c589443e
Thareeq <emuajiz@gmail.com>
2022-02-04 20:48:44 +0700

    enable detect

--- route/food.js
+++ route/food.js
@@ -32,11 +32,11 @@ exports.foodRoute = (app) => {
 
     route.get('/', handler.Catcher(controller.index))
     route.post('/', validateBody(schemas.create), handler.Catcher(controller.create))
-    // route.post('/setThumbnail/:id', multer({ storage: getDiskStorage('../uploads/thumbnail/food'), limits: imageLimits }).single('thumbnail'), handler.Catcher(controller.setThumbnail))
+    route.post('/setThumbnail/:id', multer({ storage: getDiskStorage('../uploads/thumbnail/food'), limits: imageLimits }).single('thumbnail'), handler.Catcher(controller.setThumbnail))
     route.put('/:id', validateBody(schemas.create), handler.Catcher(controller.update))
     route.delete('/:id', handler.Catcher(controller.delete))
 
-    // route.post('/upload',
-    //     // auth(),
-    //     multer({ storage: getDiskStorage('../uploads/foods'), limits: imageLimits }).single('image'), handler.Catcher(controller.detect))
+    route.post('/upload',
+        // auth(),
+        multer({ storage: getDiskStorage('../uploads/foods'), limits: imageLimits }).single('image'), handler.Catcher(controller.detect))
 }
